f4699644acba3bab6d988c54e10c2843ad74e5f2
lorraine12 <35647960+lorraine12@users.noreply.github.com>
2018-04-12 19:38:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
 app.get('/loaderio-1a00e393534e86a609de67c2022a2f0c.txt', function(request, response) {
   response.send('loaderio-1a00e393534e86a609de67c2022a2f0c')
 })
-x
+
+app.get('/loaderio-1a00e393534e86a609de67c2022a2f0c/', function(request, response) {
+  response.send('loaderio-1a00e393534e86a609de67c2022a2f0c')
+})
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
